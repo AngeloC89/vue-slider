1c0b1868ee6069b3cdc4d47d39e5b1082a0b6e16
AngeloC89 <Angelociulla89@gmail.com>
2024-03-28 15:59:29 +0100

    added methods for next and prev

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 import { slides } from "./data.js";
 
 
-const{createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
             slides,
             activeIndexSlides: 0,
 
-         
+
         }
     },
     methods: {
-        
-          
+        prevClick() {
+            if (this.activeIndexSlides > 0) {
+                this.activeIndexSlides--;
+            } else {
+                this.activeIndexSlides = slides.length - 1;
+            }
+
         },
-    mounted(){
+        nextClick() {
+            if (this.activeIndexSlides < slides.length - 1) {
+                this.prevClickactiveIndexSlides++;
+            } else {
+                this.addEventListener = 0;
+            }
+
+        },
+
+    },
+    mounted() {
+
 
     },
 }).mount('#app');
